3eb33e5b72bf8b70bf6ecb67733b1804ad52ad31
jmsnthnp <jmsnthnp@gmail.com>
2024-09-22 01:28:48 +0800

    Completed Prep 3 Activity

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        //Console.Write("What is the magic number? ");
+        //int magicNumber = int.Parse(Console.ReadLine());
+
+        string restart = "yes";
+
+        while (restart == "yes")
+        {
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1, 101);
+
+            int guess = 0;
+
+            while (guess != magicNumber)
+            {
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+
+                if (guess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else if (guess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                }
+            }
+
+            Console.Write("Play again? ");
+            restart = Console.ReadLine();
+        }
     }
 }
