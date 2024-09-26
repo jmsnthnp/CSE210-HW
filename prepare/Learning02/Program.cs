483883db09c0cef3cc61e677aa8f8546606aca1e
jmsnthnp <jmsnthnp@gmail.com>
2024-09-26 15:38:15 +0800

    Did Learning02

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Janitor";
+        job1._company = "Samsung";
+        job1._startYear = 1999;
+        job1._endYear = 2002;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Rocketman";
+        job2._company = "SpaceX";
+        job2._startYear = 1500;
+        job2._endYear = 1600;
+
+        Resume myResume = new Resume();
+        myResume._name = "James Nathan Pecundo";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
